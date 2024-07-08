92e295152855e0fd8e9e4245e4bdf3fc33f83068
JackeGould <jmgould913@gmail.com>
2024-07-08 19:53:32 -0400

    Home page layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,22 @@
 import './Home.css';
 
 function Home() {
-  // return (
-    // <div className="aboutme-container pt-4 pb-5">
-    //   <section className="content">
-    //     <article id="about-me" className="about-me">
-    //       <h2 className="section-title">About Me</h2>
-
-    //       <div className="about-me-text">
-    //         <p>
-    //           Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-    //           tenetur maiores, dolor iusto dolorum ullam, natus deleniti blanditiis
-    //           impedit suscipit sed magnam alias in, repellat expedita hic explicabo
-    //           architecto soluta. About us Lorem ipsum, dolor sit amet consectetur
-    //           adipisicing elit. Velit voluptate exercitationem quaerat pariatur
-    //           mollitia, excepturi, voluptatem eveniet a dolor nobis ex veniam totam
-    //           nostrum temporibus ad omnis nam rerum eligendi.
-    //         </p>
-    //         <p>
-    //           Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-    //           tenetur maiores, dolor iusto dolorum ullam, natus deleniti blanditiis
-    //           impedit suscipit sed magnam alias in, repellat expedita hic explicabo
-    //           architecto soluta. About us Lorem ipsum, dolor sit amet consectetur
-    //           adipisicing elit. Velit voluptate exercitationem quaerat pariatur
-    //           mollitia, excepturi, voluptatem eveniet a dolor nobis ex veniam totam
-    //           nostrum temporibus ad omnis nam rerum eligendi.
-    //         </p>
-    //       </div>
-    //     </article>
-    //   </section>
-    // </div>
-  // );
+  return (
+    <div className="main">
+      <div className="header">
+        <h1 className="year">2024</h1>
+        <h1 className="project">Portfolio</h1>
+      </div>
+      <div className='line'></div>
+      <div className="title">
+      <h3>Full Stack<br />Design Studio</h3>
+        
+      </div>
+      <div className='line2'></div>
+      <div className='banner'>
+        <h1 className='name'>Jacke <br />Gould</h1></div>
+    </div>
+  );
 }
 
 export default Home;
